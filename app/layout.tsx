8a73d8b8a17acddda520970cc406ac40605603d3
Mahesh mestri <ermapshwork@gmail.com>
2024-10-31 12:22:45 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" suppressHydrationWarning>
+       <head>
+        <link rel="icon" href="/fav.png" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,52 @@
-import Approach from "@/components/Approach";
-import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/floating-navbar";
+// "use client";
+
+// import { navItems } from "@/data";
+
+// import Hero from "@/components/Hero";
+// import Grid from "@/components/Grid";
+// import Footer from "@/components/Footer";
+// import Clients from "@/components/Clients";
+// import Approach from "@/components/Approach";
+// import Experience from "@/components/Experience";
+// import RecentProjects from "@/components/RecentProjects";
+// import { FloatingNav } from "@/components/ui/floating-navbar";
+
+// const Home = () => {
+//   return (
+//     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+//       <div className="max-w-7xl w-full">
+//         <FloatingNav navItems={navItems} />
+//         <Hero />
+//         <Grid />
+//         <RecentProjects />
+//         <Clients />
+//         <Experience />
+//         <Approach />
+//         <Footer />
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default Home;
+
+"use client";
+
 import { navItems } from "@/data";
 
-export default function Home() {
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+
+const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full h-full">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
@@ -23,4 +58,6 @@ export default function Home() {
       </div>
     </main>
   );
-}
+};
+
+export default Home;
